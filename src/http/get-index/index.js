48b3dcc8c316a40129381898276cb0a4439ab3bb
Ely Kahn <ely.kahn@gmail.com>
2021-03-21 03:18:32 -0500

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -7,26 +7,26 @@ exports.handler = async function Index () {
     /**
      * Basic bio
      */
-    fullname: 'Your Name', // ←  Start by adding your name!
-    title: 'My personal site!',
-    occupation: 'Artist & Photographer',
-    location: 'West Glacier, MT',
+    fullname: 'ely kahn', // ←  Start by adding your name!
+    title: 'digIn begin!',
+    occupation: 'designer, dev & creative technologist',
+    location: 'Little Rock',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
 
     /**
      * Contact / social
      * - Comment out any item below to remove it from your page
      */
-    email: 'your@email.com',
-    twitter: 'yourTwitter',
-    linkedin: 'your-linkedin-name',
-    instagram: 'yourInsta',
-    facebook: 'your-facebook-name',
+    email: 'ely@email.com',
+    twitter: 'creativ_ely',
+    linkedin: 'ely-kahn',
+    instagram: 'creativ_ely',
+    //facebook: 'your-facebook-name',
 
     /**
      * Layout
      */
-    photographer: 'Ivana Cajina',
+    photographer: 'Snappy Camera',
     service: 'Unsplash',
     credit: 'https://unsplash.com/@von_co',
     image: arc.static('background.jpg', {stagePath: false})
